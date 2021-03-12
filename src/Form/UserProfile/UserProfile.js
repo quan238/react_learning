500f71ace802c63ca30e733e6a3f050e0214a03a
Quan <quantran2381@gmail.com>
2021-03-13 00:17:49 +0700

    cre layout input

---
+++ src/Form/UserProfile/UserProfile.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import "./UserProfile.css";
+export default class UserProfile extends Component {
+  render() {
+    return (
+      <div
+        className="container-fluid "
+        style={{
+          backgroundColor: "#EEE",
+          position: "relative",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <form className="w-50 bg-white m-5 p-5">
+          <h1 className="text-center mt-0">User Profile</h1>
+
+          <div className="row">
+            <div className="col-6">
+              <div className="group">
+                <input type="text" required name="firstName" />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>firstName</label>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="group">
+                <input type="text" required name="lastName" />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>lastName</label>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="group">
+                <input type="text" required name="userName" />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>userName</label>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="group">
+                <input type="text" required name="email" />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>email</label>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="group">
+                <input type="password" required name="password" />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>password</label>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="group">
+                <input type="text" required name="passwordConfirm" />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>passwordConfirm</label>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <button className="btn text-white w-100 bg-dark " style={{fontSize:17}}>SUBMIT</button>
+              </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
